df33687f29d0ee8a6343ac10ee86ab5d7acf5b48
UTAGAWA Kiki <utagawa.kiki@gmail.com>
2015-09-06 18:34:20 +0900

    Add explanations to example.py

--- example.py
+++ example.py
@@ -8,4 +8,9 @@ api = croudia.API(CONSUMER_KEY, CONSUMER_SECRET)
 webbrowser.open(api.get_authorization_url())
 api.request_access_token(code=input('code : '))
 
+# Whisper
 api.post('2/statuses/update.json', {'status': 'Hello, Croudia for Python!'})
+
+# Get whispers in your timeline
+for s in api.get('2/statuses/home_timeline.json'):
+    print(s)
